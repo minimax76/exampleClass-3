31828e736a97e27e668268f7aaa4fa7581e7a3aa
Massimo Minighin <info@studiominighin.com>
2013-01-21 23:30:25 -0800

    Create Prodottto.php

---
+++ Prodottto.php
@@ -0,0 +1,12 @@
+<?php
+  class Prodotto
+	{
+		// Proprietà come variabili con attributo pubblico
+			public $id;
+			public $marca;
+			public $descrizione;
+			public $prezzo_unitario;
+			public $catalogo;
+  		// Metodi come function
+	}
+?>
