700f9b557d00e4958a48bb484f41e4ac1367f2d5
Massimo Minighin <info@studiominighin.com>
2013-01-21 23:33:52 -0800

    Create TavoloAllungabile.php

---
+++ TavoloAllungabile.php
@@ -0,0 +1,9 @@
+<?php
+    include('Tavolo.php');
+  class TavoloAllungabile extends Tavolo
+	{
+		// Proprietà come variabili con attributo pubblico
+		public $allungabile;
+  		// Metodi come function
+	}
+?>
