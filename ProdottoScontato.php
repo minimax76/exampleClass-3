351fb640a26901f23f291e16a705da682cdd6b6e
Massimo Minighin <info@studiominighin.com>
2013-01-21 23:32:53 -0800

    Create ProdottoScontato.php

---
+++ ProdottoScontato.php
@@ -0,0 +1,11 @@
+<?php
+  
+	class ProdottoScontato extends Prodotto	
+	{
+		// Proprietà come variabili con attributo pubblico
+			public $modello;
+			protected $categoria_statistica;
+			const PercentualeSconto = 10;
+  		// Metodi come function
+	}
+?>
