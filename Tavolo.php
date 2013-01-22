9e4d20b85e45f72a03ff0f67a3bfa31c2e6dfca1
Massimo Minighin <info@studiominighin.com>
2013-01-21 23:33:34 -0800

    Create Tavolo.php

---
+++ Tavolo.php
@@ -0,0 +1,13 @@
+<?php
+  class Tavolo
+	{
+		// Proprietà come variabili con attributo pubblico
+		public $larghezza;
+		public $lunghezza;
+		public $altezza;
+    	public $essenza;
+    	public $marca;
+
+  		// Metodi come function
+	}
+?>
