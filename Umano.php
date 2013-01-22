2ae6c7abd41397367557db564569569851e0f0b1
Massimo Minighin <info@studiominighin.com>
2013-01-21 23:34:09 -0800

    Create Umano.php

---
+++ Umano.php
@@ -0,0 +1,12 @@
+<?php
+  class Umano
+	{
+		// Proprietà come variabili con attributo pubblico e privato
+			public $razza;
+		//	costruttore della classe			
+			function __construct($r)
+			{
+				$this->razza = $r;
+			}
+	}
+?>
